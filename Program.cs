df1633113bd2458a4b088f61a83a43f0cc5372c1
Philip Seisman <Philip.seisman@gmail.com>
2024-11-05 11:23:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CCAD_Assignment1_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# Sharp program to print the result of dividing two numbers.
+            
+            Console.WriteLine("Please type in two numbers below: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double diff = Convert.ToDouble(num1 / num2);
+            Console.WriteLine("The difference of your two numbers is: " + diff);
+        }
+    }
+}
